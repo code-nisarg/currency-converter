acf2bd09c627d9d1dc53a57571e364eae3f16706
Nisarg <nisarganand541@gmail.com>
2025-07-20 19:07:28 +0530

    gg

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import React from 'react'
-
+import InputBox from './components/input.js';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
